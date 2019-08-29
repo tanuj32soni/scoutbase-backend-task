651475c680c455a507de457e1c9e47b5ce897a2a
Tanuj Soni <tanuj32soni@gmail.com>
2019-08-29 21:29:21 +0530

    feat: Added scoutbase_rating for logged in user

--- app/resolvers/movie.js
+++ app/resolvers/movie.js
@@ -1,11 +1,12 @@
 const _ = require('lodash')
 const Movie = require('../models').Movie
 
-const MovieResolver = async () => {
-  const movies = await Movie.findAll({
-    include: ['crewMembers'],
-    attributes: { exclude: ['scoutbase_rating'] }
-  })
+const MovieResolver = async (parent, argument, { user }) => {
+  const options = { include: ['crewMembers'] }
+  if (!user) {
+    options.attributes = { exclude: ['scoutbase_rating'] }
+  }
+  const movies = await Movie.findAll(options)
   const response = _.forEach(movies, movie => {
     const [directors, actors] = _.partition(movie.crewMembers, ['role', 'DIRECTOR'])
     return _.assign(movie, { directors, actors })
