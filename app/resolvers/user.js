a449d1dc663ae5c4478692defade345282dc14b0
Tanuj Soni <tanuj32soni@gmail.com>
2019-08-29 16:52:40 +0530

    fix: Handled Unique Username issue

--- app/resolvers/user.js
+++ app/resolvers/user.js
@@ -1,9 +1,18 @@
+const { of } = require('await-of')
+const { UserInputError } = require('apollo-server-express');
+
 const User = require('../models').User
 const { generateToken } = require('../util')
 
 const CreateUserResolver = async (parent, { username, password }) => {
   const token = generateToken(50)
-  const user = await User.create({ username, password, token });
+  const [user, error] = await of(User.create({ username, password, token }));
+
+  if(error.name === 'SequelizeUniqueConstraintError') {
+    if(error.errors && error.errors[0] && error.errors[0].message == 'username must be unique')
+      throw new UserInputError('Username already exists!')
+    throw new UserInputError('Incorrect Information Entered')
+  }
 
   return {
     token: generateToken(50),
